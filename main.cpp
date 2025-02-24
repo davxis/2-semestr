4f48bf872b4c7736f72d980825440a72f2006ff7
FedorT <ftekanov@bk.ru>
2025-02-24 08:56:25 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "class.h"
+
+int main(void)
+{
+    Person persona1("ivanov", "ivan", "ivanovich");
+    Person person2("ivanova", "Alina", "ivanovna");
+
+
+    persona1.Show();
+    persona1.Show_formal();
+
+    person2.Show();
+    person2.Show_formal();
+
+
+    return 0;
+}
