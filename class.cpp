a06b264636458e245566b5ca796814ec2f1eb8ab
FedorT <ftekanov@bk.ru>
2025-03-10 10:56:16 +0300

    Create class.cpp

---
+++ class.cpp
@@ -0,0 +1,17 @@
+#inckude class.h
+class Angle {
+private:
+    Point a, b, c;
+public:
+    // angle between `ba` and `bc`. Angle ABC
+    Angle(Point a, Point b, Point c) : a(a), b(b), c(c) {}
+
+    // Возвращает true, если угол ABC - прямой.
+    bool isRight() {
+        Point ba(a.getX() - b.getX(), a.getY() - b.getY());
+        Point bc(c.getX() - b.getX(), c.getY() - b.getY());
+        double scalarProduct = ba.getX() * bc.getX() + ba.getY() * bc.getY();
+        double epsilon = 0.0000001;
+        return abs(scalarProduct) < epsilon;
+    }
+};
