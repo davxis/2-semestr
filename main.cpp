7e8ce53b6703620426530da0751f88885ce8752b
FedorT <ftekanov@bk.ru>
2025-04-05 13:21:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@
 #include "Point.h"
 #include "Angle.h"
 #include "Rectangle.h"
+#include <cstdlib> // exit()
 
 using namespace std;
+
+Point readPoint();
 
 /*
  * @brief Основная функция программы.
@@ -17,55 +20,50 @@ int main() {
     Point a, b, c;
 
     cout << "A (x, y): ";
-    cin >> a;
-    if (!cin) {
-        cerr << "number expected\n";
-        return 1;
-    }
-
+    a = readPoint();
+    
     cout << "B (x, y): ";
-    cin >> b;
-    if (!cin) {
-        cerr << "number expected\n";
-        return 1;
-    }
+    b = readPoint();
 
     cout << "C (x, y): ";
-    cin >> c;
-    if (!cin) {
-        cerr << "number expected\n";
-        return 1;
-    }
+    c = readPoint();
 
     // Вывод введённых точек.
     cout << "A = " << a << endl;
     cout << "B = " << b << endl;
     cout << "C = " << c << endl;
 
-    Rectangle* rect = nullptr;
-
+    double radius;
+    double square;
     try {
-        rect = new Rectangle(a, b, c);
+        Rectangle rect(a, b, c);
+
+        // Вычисляем радиус окружности, описанной около прямоугольника
+        radius = rect.getCircumscribedCircleRadius();
+
+        // Вычисляем площадь круга
+        square = rect.getCircumscribedCircleSquare();
     } catch (const char* erMsg) {
         cerr << "Error: " << erMsg << endl;
-        if (rect) {
-            delete rect;
-        }
         return 1;
     }
 
-    // Вычисляем радиус окружности, описанной около прямоугольника
-    double radius = rect->getCircumscribedCircleRadius();
-
-    // Вычисляем площадь круга
-    double square = M_PI * radius * radius;
 
     // Вывод ответа
     cout << "Radius = " << radius << endl;
     cout << "Square = " << square << endl;
 
-    // Очистка памяти на куче
-    delete rect;
-
     return 0;
+}
+
+// readPoint считывает точку с устройства ввода по умолчанию.
+// Если не удалось считать, то происходит выход из программы и сообщение об ошибке.
+Point readPoint() {
+    Point resultPoint;
+    cin >> resultPoint;
+    if (!cin) {
+        cerr << "number expected\n";
+        exit(EXIT_FAILURE);
+    }
+    return resultPoint;
 }
